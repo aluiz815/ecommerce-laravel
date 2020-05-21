7831738dc5ece14e762d72bd6d12c7a6b1e5fdcc
Andre Luiz <aluiz815@gmail.com>
2020-05-21 15:34:14 -0300

    Product Controller and HomeView

--- database/factories/ProdutoFactory.php
+++ database/factories/ProdutoFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Produto::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->sentence(2),
+        'description' => $faker->sentence(20),
+        'price' => $faker->numberBetween(100, 5000),
     ];
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<div class="container text-center">
+    <h2>Produtos</h2>
+    <div class="row">
+    @foreach ($produtos as $produto)
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="col-4">
+     <div class="card">
+         <img class="card-img-top" src="{{ asset('default.jpg') }}" alt="Card image cap">
+         <div class="card-body">
+             <h4 class="card-title">Title</h4>
+             <p class="card-text">Text</p>
+         </div>
+         <div class="card-body">
+             <a href="#" class="card-link">Adicionar ao carrinho</a>
+         </div>
+     </div>
+    </div>
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+    @endforeach
     </div>
 </div>
 @endsection
